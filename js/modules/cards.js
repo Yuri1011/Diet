b968c4e380e1556c94890bcc611256351243c257
Yuri <dog-pit10@yandex.ru>
2021-03-25 18:36:49 +0300

    added modified module.

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -43,12 +43,12 @@ function cards() {
         }
     }
 
-    // getResource('http://localhost:3000/menu')
-    getResource('https://github.com/Yuri1011/Diet/blob/master/db.json/menu').then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
         });
-    });
 
 }
 
